19a0df56c57392eb25d150d4b1cd1acc428010a8
anuragrai2210 <63251556+anuragrai2210@users.noreply.github.com>
2020-08-26 19:32:36 +0530

    Add files via upload

---
+++ precache-manifest.9cdc580c0ce1969ed29c89e4907e94b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0385267731b42ddd3d8",
+    "url": "/app-co/static/css/main.b6d67c44.chunk.css"
+  },
+  {
+    "revision": "f0385267731b42ddd3d8",
+    "url": "/app-co/static/js/main.d2ea6111.chunk.js"
+  },
+  {
+    "revision": "be6ebceb806b1671429d",
+    "url": "/app-co/static/js/runtime~main.a83729cd.js"
+  },
+  {
+    "revision": "fecf7128542eb8bf328d",
+    "url": "/app-co/static/css/2.0cf95cbc.chunk.css"
+  },
+  {
+    "revision": "fecf7128542eb8bf328d",
+    "url": "/app-co/static/js/2.c3fb3c30.chunk.js"
+  },
+  {
+    "revision": "ac2fda78468de21f720cdc9638c43912",
+    "url": "/app-co/static/media/check.ac2fda78.svg"
+  },
+  {
+    "revision": "f6cbdb0f369eb4d1bc6a963e3cc1e5aa",
+    "url": "/app-co/static/media/slide_5.f6cbdb0f.jpg"
+  },
+  {
+    "revision": "8decd4b4dc1658029a06de30879028f7",
+    "url": "/app-co/static/media/slide_7.8decd4b4.jpg"
+  },
+  {
+    "revision": "7defef012a65881465c5f7b46f3dad21",
+    "url": "/app-co/static/media/logo.7defef01.svg"
+  },
+  {
+    "revision": "30dc6217141fe7b3e427f5300fe81578",
+    "url": "/app-co/static/media/chevron-down.30dc6217.svg"
+  },
+  {
+    "revision": "11c5cfe4160f0b718e8d52b4ecb34f16",
+    "url": "/app-co/static/media/vector-smart-object.11c5cfe4.png"
+  },
+  {
+    "revision": "b972a1530bff0c289fc423b022f42e8d",
+    "url": "/app-co/static/media/rectangle-6.b972a153.png"
+  },
+  {
+    "revision": "efa274072c383ad28554f0be18dc23c8",
+    "url": "/app-co/static/media/slide_2.efa27407.jpg"
+  },
+  {
+    "revision": "0bee2242b4cfd0a8f5f2b4a4d6d6ed83",
+    "url": "/app-co/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/app-co/precache-manifest.0e144e60c1db2c0db7e3b1b2625bb336.js"
+  "/app-co/precache-manifest.9cdc580c0ce1969ed29c89e4907e94b4.js"
 );
 
 workbox.clientsClaim();
