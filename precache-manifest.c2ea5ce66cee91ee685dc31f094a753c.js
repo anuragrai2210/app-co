b93917c86d485f44b91a495b3ac9704478f12f46
anuragrai2210 <63251556+anuragrai2210@users.noreply.github.com>
2020-08-26 18:33:12 +0530

    fix

---
+++ precache-manifest.c2ea5ce66cee91ee685dc31f094a753c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "093835abb1751e4bdb14",
+    "url": "/app-co/static/css/main.a4c9b1cd.chunk.css"
+  },
+  {
+    "revision": "093835abb1751e4bdb14",
+    "url": "/app-co/static/js/main.c25fe6d4.chunk.js"
+  },
+  {
+    "revision": "be6ebceb806b1671429d",
+    "url": "/app-co/static/js/runtime~main.a83729cd.js"
+  },
+  {
+    "revision": "9c810aacf08e38f9437e",
+    "url": "/app-co/static/css/2.7657aa25.chunk.css"
+  },
+  {
+    "revision": "9c810aacf08e38f9437e",
+    "url": "/app-co/static/js/2.0c30a895.chunk.js"
+  },
+  {
+    "revision": "ac2fda78468de21f720cdc9638c43912",
+    "url": "/app-co/static/media/check.ac2fda78.svg"
+  },
+  {
+    "revision": "7defef012a65881465c5f7b46f3dad21",
+    "url": "/app-co/static/media/logo.7defef01.svg"
+  },
+  {
+    "revision": "30dc6217141fe7b3e427f5300fe81578",
+    "url": "/app-co/static/media/chevron-down.30dc6217.svg"
+  },
+  {
+    "revision": "11c5cfe4160f0b718e8d52b4ecb34f16",
+    "url": "/app-co/static/media/vector-smart-object.11c5cfe4.png"
+  },
+  {
+    "revision": "4d1df2559575bce3f555724f5978bb30",
+    "url": "/app-co/index.html"
+  }
+];
