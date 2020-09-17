5738163b83291ffc7c47bd21167f725bf445f468
anuragrai2210 <63251556+anuragrai2210@users.noreply.github.com>
2020-09-17 18:15:40 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/app-co/precache-manifest.a1d869ebd1288f6ebc228fb8641f632c.js"
+  "/app-co/precache-manifest.68d462490a9aeeb0ac9f02c727f0abc1.js"
 );
 
 workbox.clientsClaim();
